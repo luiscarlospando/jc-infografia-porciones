a6e16443f6e39de0725331991112bcd780caebf8
Luis Carlos Pando <luiscarlos@luiscarlospando.net>
2018-05-15 13:12:01 -0600

    Se agregaron elementos de portada y se animaron

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,11 +15,43 @@ $(document).ready(function() {
 	});
 
 	// animate.css
-	var index = 0;
-	var total = $(".media").length;
+	// var index = 0;
+	// var total = $(".media").length;
 
-	$(".animar").click(function () {
-		$(".media").eq(index).addClass("current animated fadeInUp");
-		index = (index + 1) % total;
-	});
+	// $(".animar").click(function () {
+	// 	$(".media").eq(index).addClass("current animated fadeInDown");
+	// 	index = (index + 1) % total;
+	// });
+
+	setTimeout(function () {
+		$('#queso').show().addClass('animated fadeInDown');
+	}, 1500);
+
+	setTimeout(function () {
+		$('#miel').show().addClass('animated fadeInDown');
+	}, 2000);
+
+	setTimeout(function () {
+		$('#banana').show().addClass('animated fadeInDown');
+	}, 2500);
+
+	setTimeout(function () {
+		$('#pan').show().addClass('animated fadeInDown');
+	}, 3000);
+
+	setTimeout(function () {
+		$('#brocoli').show().addClass('animated fadeInDown');
+	}, 3500);
+
+	setTimeout(function () {
+		$('#pescado').show().addClass('animated fadeInDown');
+	}, 4000);
+
+	setTimeout(function () {
+		$('#mano').show().addClass('animated fadeIn');
+	}, 5000);
+
+	setTimeout(function () {
+		$('#porciones').show().addClass('animated fadeIn');
+	}, 6000);
 });
