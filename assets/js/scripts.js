62aee868a51901f708c5d664b9b30b5d58bded6f
Luis Carlos Pando <luiscarlos@luiscarlospando.net>
2018-05-16 12:59:45 -0600

    Se alineó portada al fondo del viewport, se quitaron márgenes innecesarios arriba y abajo y se agregó un dropdown de ejemplo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,15 +3,14 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		navigation: true,
 		showActiveTooltip: true,
-		paddingTop: '50px',
-		paddingBottom: '50px',
 		recordHistory: false,
 		anchors: ['pagina-1', 'pagina-2', 'pagina-3','pagina-4', 'pagina-5'],
 		menu: '.main-nav ul',
 		fixedElements: '.header, .footer',
 		paddingTop: '30px',
-		paddingBottom: '55px',
-		scrollOverflow: true
+		// paddingBottom: '55px',
+		scrollOverflow: true,
+		controlArrows: false
 	});
 
 	// animate.css
